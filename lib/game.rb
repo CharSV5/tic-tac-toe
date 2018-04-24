74e0fe09184e3b61ff3e7f8569a6e3bbcddbb960
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-24 10:16:23 +0100

    switch player turn

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,35 @@ require_relative 'player'
 
 
 class Game
-  attr_reader :x, :o
+  attr_reader :x, :o, :turn, :board
+
+  def initialize
+    @turn = nil
+  end
 
   def create_players
     @x = Player.new
     @o = Player.new
   end
+
+  def create_board
+    @board = Board.new
+  end
+
+  def start_game
+    @turn = [x, o].sample
+  end
+
+  def switch_player_turn
+    if @turn == @x
+      @turn = @o
+    else
+      @turn = @x
+    end
+  end
+
+  def player_x_turn?
+    @turn == @x
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,24 @@
 require 'game'
 
 describe 'Game' do
+  subject {Game.new}
   describe '#create_players' do
-      subject {Game.new}
-      it 'creates a new player called x' do
-        subject.create_players
-        expect(subject.o).to be_a Player
-      end
-      it 'creates a new player called o' do
-        subject.create_players
-        expect(subject.o).to be_a Player
-      end
+    it 'creates a new player called x' do
+      subject.create_players
+      expect(subject.o).to be_a Player
+    end
+    it 'creates a new player called o' do
+      subject.create_players
+      expect(subject.o).to be_a Player
+    end
   end
+  describe '#switch_player_turn' do
+    let(:turn) { double("turn") }
+    it 'switches the turn of the player' do
+      allow(turn).to receive(:x)
+      subject.switch_player_turn
+      expect(subject.turn).to eq subject.o
+    end
+  end
+  describe ''
 end
