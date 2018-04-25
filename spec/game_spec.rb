20e25c857beb7d5381739404ecc62ebf4f212441
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-25 16:59:59 +0100

    refactored tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,25 +7,21 @@ describe 'Game' do
   end
   describe '#create_players' do
     it 'creates a new player called x' do
-
       expect(subject.o).to be_a Player
     end
     it 'creates a new player called o' do
-
       expect(subject.o).to be_a Player
     end
   end
   describe '#switch_player_turn' do
     it 'switches the turn of the player' do
-
       subject.switch_player_turn
       expect(subject.turn).to eq subject.o
     end
   end
-  # Not testing in isolation
+
   describe '#play' do
     it 'lets a player play a turn' do
-
       board = double('board', start: [1, 2, 3, 4, 5, 6, 7, 8, 9])
       subject.create_board(board)
       subject.x.create_record
@@ -35,9 +31,11 @@ describe 'Game' do
   end
   describe '#winner?' do
     it 'knows when someone wins' do
-
-      # board = double('board', start: [1, 2, 3, 4, 5, 6, 7, 8, 9])
-      subject.create_board
+      board = double('board')
+      allow(board).to receive_messages(start: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        combinations: [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8],
+                         [3, 6, 9], [1, 5, 9], [3, 5, 7]])
+      subject.create_board(board)
       subject.x.create_record
       subject.play(1)
       subject.play(4)
